b264ef8557a7ca42d6ee1455935f37ee4a93e26b
andreiLaptop <ahrudan@gmail.com>
2018-10-28 11:05:53 +0200

    Trimis de pe laptop

---
+++ Boosting/Controllers/EmailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Boosting.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Sent()
+        {
+            return View();
+        }
+    }
+}
